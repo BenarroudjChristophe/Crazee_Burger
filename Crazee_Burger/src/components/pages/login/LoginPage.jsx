0c268c5993f540b8af73d5358751d7d103a107e3
bingojumbo <binogjumbo@yahoo.com>
2023-08-07 16:09:03 +0200

    ✨ F03: Styled Login Page

--- Crazee_Burger/src/components/pages/login/LoginPage.jsx
+++ Crazee_Burger/src/components/pages/login/LoginPage.jsx
@@ -1,10 +1,33 @@
+import { styled } from "styled-components";
 import LoginForm from "./LoginForm";
-import "./LoginPage.css";
+import divBeforeImage from "../../../assets/div_before.png";
+import CrazeeBurger from "../CrazeeBurger";
+
 
 export default function LoginPage() {
 	return (
-		<>
+	
+		<LoginPageStyle>
+			<CrazeeBurger />
 			<LoginForm />
-		</>
+		</LoginPageStyle>
+		
 	);
 }
+
+const LoginPageStyle = styled.div`
+background: url(${divBeforeImage});
+position: absolute;
+width: 94.5rem;
+height: 61.375rem;
+/* opacity: 0.5; */
+/* background: #000; */
+mix-blend-mode: darken;
+/* box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25); */
+
+
+
+
+
+`;
+
